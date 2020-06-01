bc32184d23dd345dba331138bd68cbc36de2a3a2
stephenlai <stephenlai2015@gmail.com>
2020-06-01 17:57:18 +0800

    edit css

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ new Vue({
   },
   mounted() {
     this.invoiceList = JSON.parse(localStorage.getItem('invoice-number')) || []
-    this.isActive = JSON.parse(localStorage.getItem('menu-state')) || 'price'
-    this.isChn = JSON.parse(localStorage.getItem('chinese-state')) || true
+    this.isActive = JSON.parse(localStorage.getItem('menu-state')) 
+    this.isChn = JSON.parse(localStorage.getItem('chinese-state')) 
 
     if (this.isActive == 'price') this.showPrice()
     if (this.isActive == 'admin') this.showAdmin()
